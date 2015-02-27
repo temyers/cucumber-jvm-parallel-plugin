b3f7c269ae59b53a1f85222651d441f11188f05c
temyers <tim.myerscough@permeance.com.au>
2015-02-27 03:03:09 +0000

    fix issue#1 - build fails on Windows machine due to incorrect file path separator

--- src/main/java/com/github/timm/cucumber/generate/GenerateRunnersMojo.java
+++ src/main/java/com/github/timm/cucumber/generate/GenerateRunnersMojo.java
@@ -2,12 +2,12 @@ package com.github.timm.cucumber.generate;
 
 /*
  * Copyright 2001-2005 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -178,7 +178,8 @@ public class GenerateRunnersMojo extends AbstractMojo {
      *            The feature file
      */
     private void setFeatureFileLocation(final File file) {
-        featureFileLocation = file.getPath().replace(featuresDirectory.getPath(), featuresDirectory.getName());
+        featureFileLocation = file.getPath().replace(featuresDirectory.getPath(), featuresDirectory.getName())
+                .replace(File.separatorChar, '/');
     }
 
     private void writeContentFromTemplate(final Writer writer) {
