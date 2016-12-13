5d167a0b650405a291b4b3e4477d05f71470a4ee
Markus Heberling <markus.heberling@hpe.com>
2016-12-13 16:36:06 +0100

    allow setting of packageName for runners. Closes #45

---
+++ src/it/junit/custom-package/verify.groovy
@@ -0,0 +1,51 @@
+import org.junit.Assert
+
+import static org.hamcrest.Matchers.equalToIgnoringWhiteSpace
+
+File suite01 = new File(basedir, "target/generated-test-sources/cucumber/foo/bar/Parallel01IT.java");
+File suite02 = new File(basedir, "target/generated-test-sources/cucumber/foo/bar/Parallel02IT.java");
+
+File feature1 = new File(basedir, "/src/test/resources/features/feature1.feature");
+File feature2 = new File(basedir, "/src/test/resources/features/feature2.feature");
+
+assert suite01.isFile()
+assert suite02.isFile()
+
+String expected01 =
+        """package foo.bar;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(strict = true, features = {"${feature1.absolutePath}"}, plugin = {"json:target/cucumber-parallel/1.json"},
+monochrome = false, tags = {"@complete", "@accepted"}, glue = { "foo", "bar" })
+public class Parallel01IT {
+}"""
+
+String expected02 =
+        """package foo.bar;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(strict = true, features = {"${feature2.absolutePath}"}, plugin = {"json:target/cucumber-parallel/2.json"},
+monochrome = false, tags = {"@complete", "@accepted"}, glue = { "foo", "bar" })
+public class Parallel02IT {
+}"""
+
+// Depending on the OS, listFiles can list files in different order.  The actual order of files isn't necessary
+
+if (suite01.text.contains("feature1")) {
+    Assert.assertThat(suite01.text, equalToIgnoringWhiteSpace(expected01))
+    Assert.assertThat(suite02.text, equalToIgnoringWhiteSpace(expected02))
+} else {
+    Assert.assertThat(suite02.text, equalToIgnoringWhiteSpace(expected01))
+    Assert.assertThat(suite01.text, equalToIgnoringWhiteSpace(expected02))
+}
+
