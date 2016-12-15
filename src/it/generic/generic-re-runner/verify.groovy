a08528400d60ed5cc3acd0220e3220e5a527dfcf
sugatmankar <sugatmankar@gmail.com>
2016-12-15 15:35:11 +0530

    Added Generic ReRunner

--- src/it/generic/generic-re-runner/verify.groovy
+++ src/it/generic/generic-re-runner/verify.groovy
@@ -1,5 +1,7 @@
 import org.junit.Assert
 
+import static org.hamcrest.Matchers.equalToIgnoringWhiteSpace
+
 File suite01 = new File(basedir, "target/generated-test-sources/cucumber/Parallel01IT.java");
 File suite02 = new File(basedir, "target/generated-test-sources/cucumber/Parallel02IT.java");
 
@@ -517,10 +519,10 @@ public class Parallel02IT {
 // Depending on the OS, listFiles can list files in different order.  The actual order of files isn't necessary
 
 if (suite01.text.contains("feature1")) {
-    Assert.assertThat(suite01.text, containsString(expected01))
-    Assert.assertThat(suite02.text, containsString(expected02))
+    Assert.assertThat(suite01.text, equalToIgnoringWhiteSpace(expected01))
+    Assert.assertThat(suite02.text, equalToIgnoringWhiteSpace(expected02))
 } else {
-    Assert.assertThat(suite02.text, containsString(expected01))
-    Assert.assertThat(suite01.text, containsString(expected02))
+    Assert.assertThat(suite02.text, equalToIgnoringWhiteSpace(expected01))
+    Assert.assertThat(suite01.text, equalToIgnoringWhiteSpace(expected02))
 }
 
